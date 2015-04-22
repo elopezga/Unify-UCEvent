2f81eb74261f2757bb8f904c32a67ae7d023169e
Nicholas McDonald <nmmcdona@ucsd.edu>
2015-04-22 11:27:15 -0700

    Fixed app crash. Still throwing exception however.

--- app/src/main/java/com/unify/ucevent/MainActivity.java
+++ app/src/main/java/com/unify/ucevent/MainActivity.java
@@ -19,9 +19,9 @@ public class MainActivity extends ActionBarActivity {
 
         // Enables Parse Local Datastore - Connects to UCEvent
         // Login: ssdai@ucsd.edu
-        Parse.enableLocalDatastore(this);
+        /*Parse.enableLocalDatastore(this);
         Parse.initialize(this, "62Yg7BmL5VctbBBlYDiIutmcp3NwJSIXkzOIKMTn",
-                               "0uyGE5SGTg7szwgz9ZetTzBpD5wcR2pu6vKqgOSF");
+                               "0uyGE5SGTg7szwgz9ZetTzBpD5wcR2pu6vKqgOSF");*/
 
         // Test Parse SDK
         ParseObject testObject = new ParseObject("TestObject");
