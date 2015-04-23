7c23538b06766c7de285b467f567b19cb77771ff
Nicholas McDonald <nmmcdona@ucsd.edu>
2015-04-22 19:47:52 -0700

    Added one line that i guess we need.

--- app/src/main/java/com/unify/ucevent/ParsePushApp.java
+++ app/src/main/java/com/unify/ucevent/ParsePushApp.java
@@ -6,6 +6,7 @@ import com.parse.*;
 public class ParsePushApp extends Application{
     @Override
     public void onCreate() {
+        super.onCreate();
         Parse.initialize(this, "62Yg7BmL5VctbBBlYDiIutmcp3NwJSIXkzOIKMTn", "0uyGE5SGTg7szwgz9ZetTzBpD5wcR2pu6vKqgOSF");
         ParseInstallation.getCurrentInstallation().saveInBackground();
     }
