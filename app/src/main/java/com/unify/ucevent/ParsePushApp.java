0514cb76cd133d04009dc8da32253a66a7206b95
Han Kim <kim>
2015-04-26 20:42:34 -0700

    To checkout some older version

--- app/src/main/java/com/unify/ucevent/ParsePushApp.java
+++ app/src/main/java/com/unify/ucevent/ParsePushApp.java
@@ -1,14 +1,15 @@
 package com.unify.ucevent;
 
 import android.app.Application;
-import com.parse.*;
+import com.parse.Parse;
+import com.parse.PushService;
 
 public class ParsePushApp extends Application {
     //Initializing Parse and stuff for our app
     @Override
     public void onCreate() {
-        super.onCreate();
         Parse.initialize(this, "62Yg7BmL5VctbBBlYDiIutmcp3NwJSIXkzOIKMTn", "0uyGE5SGTg7szwgz9ZetTzBpD5wcR2pu6vKqgOSF");
-        ParseInstallation.getCurrentInstallation().saveInBackground();
+        //ParseInstallation.getCurrentInstallation().saveInBackground();
+        PushService.setDefaultPushCallback(this, MainActivity.class);
     }
 }
