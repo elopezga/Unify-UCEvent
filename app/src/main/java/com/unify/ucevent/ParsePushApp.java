cdc3b78f47df914e6221bb2b8735391bda25c325
Han Kim <kim>
2015-05-03 12:50:09 -0700

    Facebook Login Done Hopely

--- app/src/main/java/com/unify/ucevent/ParsePushApp.java
+++ app/src/main/java/com/unify/ucevent/ParsePushApp.java
@@ -1,7 +1,16 @@
 package com.unify.ucevent;
 
 import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+
 import com.parse.*;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class ParsePushApp extends Application {
     //Initializing Parse and stuff for our app
@@ -18,9 +27,11 @@ public class ParsePushApp extends Application {
         Parse.initialize(this, PARSE_APP_ID, PARSE_CLIENT_KEY);
         ParseFacebookUtils.initialize(this);
 
+
         //ParseInstallation.getCurrentInstallation().saveInBackground();
 
         // Test Parse SDK
 
     }
+
 }
--- app/src/main/java/com/unify/ucevent/SampleDispatchActivity.java
+++ app/src/main/java/com/unify/ucevent/SampleDispatchActivity.java
@@ -21,12 +21,37 @@
 
 package com.unify.ucevent;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+
 import com.parse.ui.ParseLoginDispatchActivity;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class SampleDispatchActivity extends ParseLoginDispatchActivity {
 
   @Override
   protected Class<?> getTargetClass() {
-    return SampleProfileActivity.class;
+      /* For printing out the key hash
+      try {
+          PackageInfo info = getPackageManager().getPackageInfo(
+                  "com.facebook.samples.hellofacebook",
+                  PackageManager.GET_SIGNATURES);
+          for (Signature signature : info.signatures) {
+              MessageDigest md = MessageDigest.getInstance("SHA");
+              md.update(signature.toByteArray());
+              Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+          }
+      } catch (PackageManager.NameNotFoundException e) {
+
+      } catch (NoSuchAlgorithmException e) {
+
+      }*/
+
+      return SampleProfileActivity.class;
   }
 }
