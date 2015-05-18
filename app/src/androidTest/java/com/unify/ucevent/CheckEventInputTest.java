e2ecacd7e3453cde2ab541c458634c427a71da55
Han Kim <hak014@ucsd.edu>
2015-05-17 17:36:11 -0700

    Working on Testing

---
+++ app/src/androidTest/java/com/unify/ucevent/CheckEventInputTest.java
@@ -0,0 +1,30 @@
+package com.unify.ucevent;
+
+import android.test.InstrumentationTestCase;
+import android.widget.EditText;
+
+/**
+ * Created by HDK on 5/17/15.
+ */
+public class CheckEventInputTest extends InstrumentationTestCase {
+
+    public void testConfirmTitle() throws Exception {
+
+        assertEquals(false,true);
+    }
+    public void testConfirmLocation() throws Exception {
+
+    }
+    public void testConfirmContact() throws Exception {
+
+    }
+    public void testConfirmDate() throws Exception {
+
+    }
+    public void testConfirmStartTime() throws Exception {
+
+    }
+    public void testConfirmEndTime() throws Exception {
+
+    }
+}
